68bfa616d507bae036028e7a7e38b69b64409712
admin <admin@MacBook-Pro-admin.local>
2024-03-20 15:19:22 +0200

    first programs

---
+++ First meeting/main.py
@@ -0,0 +1,37 @@
+# вывод
+print("Hellow, World!")
+# int
+test = 5
+print(test)
+# float
+test1 = 2.5
+print(test1)
+test3 = print(test/test1)
+# str
+test2 = "Name"
+print(test2)
+# экранирование
+print("Он \"хороший\" человек")
+print('Он "хороший" человек')
+print('Привет\nмир')
+name = "Andrew"
+print("Имя: " + name + "!")
+old = 18
+print("Возраст: " + str(old))
+
+name = input("Введите имя: " )
+old = input("Введите возраст: " )
+print("Имя " + name + "!")
+print("Возраст " + old + "!")
+
+a = 5
+b = 10
+c = a+b
+
+for i in range(c):
+    print(i, end=" ")
+
+q=0.01
+w=1
+e=q+w
+print("\n\n" + str(e))
---
+++ First meeting/main1.py
@@ -0,0 +1,51 @@
+print(4+3)
+print("Hello, World!")
+a = 7
+b = 0
+b = -8.4
+print(a, b)
+var_a = "hello"
+print(var_a)
+a=b=c=0
+print(a,b,c)
+print(id(a), id(b), id(c))
+a,b=1,2
+print(a,b,c)
+print(id(a), id(b), id(c))
+a,b=b,a
+print(a,b,c)
+print(id(a), id(b), id(c))
+print(type(a))
+x = 5.8
+s = "Hello"
+print(type(x),type(s))
+h = 1.001
+print(type(h))
+msq = "Сообщение"
+count = 0
+arg = 0
+Arg = 0
+print(msq)
+'''
+ int 0, 1, 2, 100, 6697959484, -1, -2, -75658
+ float 6.8 -5.567, 345.546, -65467.99
+ complex
+'''
+print("2^3 = " + str(2**3))
+print("2^3^2 = " + str(2**3**2))
+print(10%4)
+print(-9%5) # -9 - (-10)
+print(-7%5) # -8- (-5)
+print(9%-5) # 9 -10
+print(-9%7)
+print(-10%-8)
+
+q=abs(-10)
+print(q)
+print(1/2)
+print(1/3)
+print(1//3)
+print("I\'m ono the round", end=' ')
+print("empty and cold.")
+num = int(input('Enter a number: '))
+print("num = " + str(num))
